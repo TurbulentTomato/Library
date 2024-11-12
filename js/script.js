ca7913dcdab18284b0d5f1557355e501e59203a2
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-12 23:26:03 +0530

    Add Book constructor

--- js/script.js
+++ js/script.js
@@ -5,13 +5,25 @@ const confirmBtn = document.querySelector(".confirm-btn");
 const bookNameEl = document.querySelector("#book-name");
 const authorNameEl = document.querySelector("#author-name");
 const bookYearEl = document.querySelector("#year");
-const coverImgEl = document.querySelector("#cover-img")
+const coverImgEl = document.querySelector("#cover-img");
 
+//opens modal
 addBookBtn.addEventListener("click", () => {
   bookModal.showModal();
 })
 
+//closes modal
 confirmBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  bookModal.close()
+  if (bookNameEl.value === "") {
+    bookModal.close();
+  }
+  let book = new Book(bookNameEl.value, authorNameEl.value, bookYearEl.value, coverImgEl.value)
 })
+
+function Book(name, author, year, coverImg) {
+  this.name = name;
+  this.author = author;
+  this.year = year;
+  this.coverImg = coverImg;
+}
