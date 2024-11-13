63f30fc76644627332285aedd2fdbd8dd56b2a53
TurbulentTomato <abhinavgangwar87@gmail.com>
2024-11-13 20:48:35 +0530

    Add functionality to toggle "read" status

--- js/script.js
+++ js/script.js
@@ -8,16 +8,18 @@ const bookYearEl = document.querySelector("#year");
 const coverImgEl = document.querySelector("#cover-img");
 const readBookEl = document.querySelector("#read");
 let books = [
-  {
-    name: "One piece",
-    author: "Oda",
-    year: 1997,
-    coverImg: "",
-    read: false
-  }
+  new Book("One Piece", "Oda", 1997, "nope", false)
 ];
 
+
 renderBooks();
+document.querySelector(".library").addEventListener("click", (event) => {
+  console.log("hello")
+  if (Array.from(event.target.classList).includes("book-read-checkbox")) {
+    console.log("inside if")
+    toggleReadCheckbox(event.target);
+  }
+})
 //opens modal
 addBookBtn.addEventListener("click", () => {
   bookModal.showModal();
@@ -47,6 +49,10 @@ function Book(name, author, year, coverImg, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+  this.read = !(this.read);
+}
+
 function addBookToLibrary(newBook) {
   books.push(newBook);
   renderBooks();
@@ -63,7 +69,8 @@ function createBookCard(book) {
   const article = `
     <article>
         <input type="checkbox" value="${(book.read)}" 
-         class="book-read-checkbox" ${(book.read) ? "checked" : "unchecked"}> 
+         class="book-read-checkbox" ${(book.read) ? "checked" : "unchecked"} 
+         data-index="${books.indexOf(book)}">
         <button type="button" class="del-book-btn">×</button>
         <div class="book">
           <img src="${book.coverImg}" alt="Cover Image" width="200px" height="300px">
@@ -79,8 +86,11 @@ function createBookCard(book) {
   return article;
 }
 
-/*Book.prototype.toggleRead = function() {
-  this.read = !(this.read);
-}*/
+function toggleReadCheckbox(checkbox) {
+  let bookIndex = Number(checkbox.dataset.index);
+  console.log(bookIndex)
+  books[bookIndex].toggleRead();
+  renderBooks();
+}
 
 
